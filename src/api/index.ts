f1d118b57146f770f9ec467f6f81b052290d3cd7
Hecker <julianheckerdev@gmail.com>
2022-01-14 21:22:55 -0500

    typos

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,8 +6,14 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
-import { decodeToken, errorHandler, notFoundHandler } from './middlewares';
+import {
+  decodeToken,
+  errorHandler,
+  notFoundHandler,
+} from './middlewares';
 import { router } from './routes';
+import { connect } from './config/database';
+import { Client, User } from './entities';
 
 export const app = express();
 
@@ -21,8 +27,26 @@ app.use(decodeToken);
 
 app.use(router);
 
-
 app.use(notFoundHandler);
 app.use(errorHandler);
+
+async function main() {
+  try {
+    const connection = await connect();
+    const result = await connection
+      .createQueryBuilder()
+      .select('user')
+      .from(User, 'user')
+      .leftJoinAndSelect('user.client', 'client')
+      .getMany();
+    // const result = await Client.find({ relations: ['user'] });
+    // const result = await User.find({ relations: ['client'] });
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
 
 // https://www.freecodecamp.org/news/how-to-write-a-production-ready-node-and-express-app-f214f0b17d8c/
