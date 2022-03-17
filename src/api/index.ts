9f9791e914a2bfef4fc99ce4ca900fafe1d39019
Hecker <julianheckerdev@gmail.com>
2022-03-17 02:36:02 -0400

    Fix cors issue

--- src/api/index.ts
+++ src/api/index.ts
@@ -20,7 +20,7 @@ export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
-app.use(cors());
+app.use(cors({ origin: true }));
 app.use(helmet());
 app.use(morgan('tiny'));
 app.use(decodeToken);
